619b95516d56dac4aab971d24cf8973220804c16
Ravindra JOB <ravindra_job@carrefour.com>
2021-06-04 18:40:19 +0200

    update readm me

---
+++ raspberry/ina219ToInflux/README.md
@@ -0,0 +1,19 @@
+# Introduction
+
+## 1.Install python
+sudo apt-get install python3-pip
+
+####.Install library
+
+
+### Check if it's ok 
+pip3 list | grep ina219
+
+### execution
+python3 demo.py
+![alt text](https://ravindrajob.blob.core.windows.net/assets/python-ina.png)
+
+## 2.Intégration InfluxDB
+
+
+## 3.Intégration Grafana
---
+++ raspberry/ina219ToInflux/ups2influxdb.py
@@ -0,0 +1,52 @@
+# IMPORT THE LIBERARY.
+from influxdb_client import InfluxDBClient, Point
+from ina219 import INA219
+from ina219 import DeviceRangeError
+SHUNT_OHMS = 0.05
+mv=float()
+current=float()
+power=float()
+shunt=float()
+
+#from influxdb_client import InfluxDBClient, Point
+username = 'ups'
+password = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+database = 'ups'
+retention_policy = 'autogen'
+bucket = f'{database}/{retention_policy}'
+client = InfluxDBClient(url='https://database.ravindra-job', token=f'{username}:{password}', org='-')
+print('*** Write Points ***')
+write_api = client.write_api()
+
+def read():
+    """Define method to read information from coulometer."""
+    ina = INA219(SHUNT_OHMS)
+    ina.configure()
+    current = "%.3f" % ina.voltage()
+    power = "%.3f" % ina.power()
+    shunt = "%.3f" % ina.shunt_voltage()
+
+    point = Point("rpi").tag("host", "rpi4").field("current", current)
+    point2 = Point("rpi").tag("host", "rpi4").field("power", power)
+    point3 = Point("rpi").tag("host", "rpi4").field("shunt", shunt)
+
+    print(point.to_line_protocol())
+    print(point2.to_line_protocol())
+    print(point3.to_line_protocol())
+    write_api.write(bucket=bucket, record=point3)
+    write_api.write(bucket=bucket, record=point2)
+    write_api.write(bucket=bucket, record=point)
+    write_api.__del__()
+
+    print("Bus Voltage: %.3f V" % ina.voltage())
+    try:
+        print("Bus Current: %.3f mA" % ina.current())
+        print("Power: %.3f mW" % ina.power())
+        print("Shunt voltage: %.3f mV" % ina.shunt_voltage())
+    except DeviceRangeError as e:
+        print(e)
+if __name__ == "__main__":
+    read()
+
+print('*** Query Points ***')
+client.close()
